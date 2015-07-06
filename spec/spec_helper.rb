d7dfca0fbc00484fafb8c038044623a237f39a1c
lkdjiin <xavier.nayrac@gmail.com>
2015-07-06 16:38:58 +0200

    Better Rspec configuration
    Play the suite with format=dot but play a single file with format=doc.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'coco'
 require 'json2'
+
+RSpec.configure do |config|
+  if config.files_to_run.one?
+    config.default_formatter = 'documentation'
+  end
+end
